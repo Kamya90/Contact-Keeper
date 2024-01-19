53c5b67c0ac4ca2c09d7207b0055f83313bcf4ea
Nagpal <k.a.nagpal@accenture.com>
2024-01-19 21:47:01 +0530

    auth

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express= require('express');
+const connectDB=require('./config/db');
 const app=express();
-const PORT= process.env.PORT || 5000;
+
+//connect DB
+connectDB();
+
+//init middleware
+app.use(express.json({extended:false}));
 
 app.get('/', (req,res) => res.json({msg:'welcome to the contact-keeper api'}));
 
@@ -9,4 +15,8 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
+const PORT= process.env.PORT || 5000;
+
 app.listen(PORT, ()=> console.log(`Server started on ${PORT}`));
+
+module.exports = app;
